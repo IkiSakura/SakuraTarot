a76016e5fb05c2cbdd38f6b80fe8c3bbd1b7afd7
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-06-29 15:17:02 +0200

    correccion archivo journal.jsx para verlo en servidor

---
+++ sakura-tarot/src/pages/journal/Journal.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header"
+
+
+
+export default function Journal() {
+
+    return (
+        <div className="journal-container">
+            <Header />
+            <h1>Journal</h1>
+            
+        </div>
+
+            )
+
+}
