30f6d6da16c30bc146996ffbfd38b151bcbe4d9e
Sylvia Llorente <sylviall81@gmail.com>
2023-07-06 23:44:21 +0200

    añadidos test corregidos

--- sakura-tarot/src/pages/home/Home.test.jsx
+++ sakura-tarot/src/pages/home/Home.test.jsx
@@ -1,56 +1,43 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import Home from './Home'
-import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
+import { beforeEach, expect } from "vitest"
+import Button from '../../components/Button/Button';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+import { createMemoryHistory } from 'history';
+
 
 
 
 describe('Component Home', () => {
-    beforeEach(() => {
-      render(
-        <MemoryRouter>
-          <Home />
-        </MemoryRouter>
-      );
-    });
-  
-
-test('increments clickCount when maxClicks is not reached', () => {
-  const maxClicks = 3;
-  let clickCount = 0;
-  const setClickCount = jest.fn((newClickCount) => {
-    clickCount = newClickCount;
+  beforeEach(() => {
+    render(
+      <Router>
+        <Home />
+      </Router>
+    );
   });
 
-  const handleCardClick = () => {
-    if (clickCount < maxClicks) {
-      setClickCount(clickCount + 1);
-    }
-  };
+  test('check click event de "ver mi lectura" Button', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Button text='Ver mi lectura' to='/reading' />
+      </Router>
+    );
 
-  const { getByTestId } = render(
-    <button onClick={handleCardClick} data-testid="card-button">
-      Click me
-    </button>
-  );
+  test('should render the "ver mi lectura" Button with the correct path', () => {
 
-  const cardButton = getByTestId('card-button');
+    const showReadingBtn = screen.getByText('Ver mi lectura');
+    expect(showReadingBtn).toBeInTheDocument();
+    fireEvent.click(showReadingBtn[0]);
 
-  fireEvent.click(cardButton);
-  expect(clickCount).toBe(1);
-  expect(setClickCount).toHaveBeenCalledWith(1);
+    expect(showReadingBtn.length).toBeGreaterThan(0);
+    expect(showReadingBtn).toHaveAttribute('to', '/reading');
+    expect(startButton.location.pathname).toBe("/reading");
 
-  fireEvent.click(cardButton);
-  expect(clickCount).toBe(2);
-  expect(setClickCount).toHaveBeenCalledWith(2);
-
-  fireEvent.click(cardButton);
-  expect(clickCount).toBe(3);
-  expect(setClickCount).toHaveBeenCalledWith(3);
-
-  fireEvent.click(cardButton);
-  expect(clickCount).toBe(3);
-  expect(setClickCount).not.toHaveBeenCalledWith(4);
-});
+  });
+})
 })
--- sakura-tarot/src/pages/reading/Reading.test.jsx
+++ sakura-tarot/src/pages/reading/Reading.test.jsx
@@ -1,41 +1,41 @@
 
 import React from 'react';
-//import { beforeEach, expect } from "vitest"
+import { beforeEach, expect } from "vitest"
 import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import {createMemoryHistory} from 'history';
+//import { BrowserRouter as Router } from 'react-router-dom';
+//import {createMemoryHistory} from 'history';
 import  '@testing-library/jest-dom/extend-expect';
 import Reading from './Reading';
 
 
 describe('Component Reading', () => {
-    beforeEach(() => {
-      render(
-        <MemoryRouter>
-          <Reading />
-        </MemoryRouter>
-      );
-    });
-  
-  test('should render the Golden circle image correctly ', () => {
-    const goldCircle = screen.getByAltText("Sakura golden card circle");
-    
-});
-
-});
-test('should render the save Button Guardar ', () => {
-    const saveButton = screen.getByText('Guardar');
-    expect(saveButton).toBeInTheDocument();
-    
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <Reading />
+      </MemoryRouter>
+    );
   });
 
+  test('should render the Golden circle image correctly', () => {
+    const goldCircle = screen.getByAltText("Sakura golden card circle");
+    expect(goldCircle).toBeInTheDocument();
+  });
+
+  test('should render the save Button Guardar', () => {
+    const saveButton = screen.getByText('Guardar');
+    expect(saveButton).toBeInTheDocument();
+  });
+/*
   test('submit elements', () => {
     const saveButton = screen.getByText('Guardar');
     fireEvent.click(saveButton[0]);
 
+    expect(saveButtonButton).toHaveAttribute('onClick', `${handleOpenModal}`);
     expect(screen.getByText('Pensamiento guardado correctamente.')).toBeTruthy();
-  });
+  });*/
+});
 
   
   
